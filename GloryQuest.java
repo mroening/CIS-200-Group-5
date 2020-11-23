134ba71f3ad5e7003337fe8ede991a7a14bf7319
Micah Roening <roening@ksu.edu>
2020-11-23 16:01:51 -0600

    Added Monster support for Input

---
+++ GloryQuest.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+
+public class GloryQuest {
+	
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		
+		System.out.print("Welcome to GLORYQUEST! Type (P)lay to play GLORYQUEST, or type (D)ev to enter Developer mode: ");
+		char titleChoice = s.nextLine().toLowerCase().charAt(0);
+		
+		if (titleChoice == 'd') {
+			try {
+				Input devMode = new Input();
+				devMode.createNewThing();
+			} catch (IOException x) {
+				System.out.println("There was an error with the file! Please check your installation and try again.");
+			}
+		} else {
+			System.out.println("You think you're a funny man, dont'cha? A real razzle-dazzler! Why don't you go try to impress a Commadore, you fish-lipped clown college drop-out.");
+		}
+		
+	}
+
+}
--- Input.java
+++ Input.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.io.*;
 
 public class Input {
 
@@ -12,19 +13,18 @@ public class Input {
 			System.out.print("Would you like to input a piece of 'Equipment', a 'Monster', a 'Choice', or a 'Trap': ");
 			choice = s.nextLine();
 			
-			if (!choice.equalsIgnoreCase("Equipment") || !choice.equalsIgnoreCase("Monster") ||
-				!choice.equalsIgnoreCase("Choice") || !choice.equalsIgnoreCase("Trap"))
+			if (choice.equalsIgnoreCase("Equipment") || choice.equalsIgnoreCase("Monster") ||
+				choice.equalsIgnoreCase("Choice") || choice.equalsIgnoreCase("Trap"))
 				validClass = true;
 			else
 				System.out.println("Please choose a valid object!");
 		}
 		
 		type = choice;
-		} // end while
 	}
 	
-	public void createNewThing () {
-		if (type.equalsIgnoreCase("Equipment") {
+	public void createNewThing () throws IOException {
+		if (type.equalsIgnoreCase("Equipment")) {
 			createEquipment();
 		} else if (type.equalsIgnoreCase("Monster")) {
 			createMonster();
@@ -40,8 +40,109 @@ public class Input {
 		
 	}
 	
-	public void createMonster () {
-		// take in input, check for validity, add it to a file
+	public void createMonster () throws IOException {
+		int health = 0;
+		int damage = 0;
+		int difficulty = 0;
+		String name = "";
+		String location = "";
+	
+		File monsterFile = new File("Monsters.txt");
+		Scanner readFile = new Scanner(monsterFile);
+		
+		// add tutorial/point to README
+		System.out.println("Welcome to Monster creation!");
+		
+		// getting a valid location for the monster
+		do {
+			System.out.print("Choose if this is a (C)ave, (F)orest, (W)asteland, or (V)illage monster: ");
+			location = s.nextLine().toLowerCase();
+			System.out.println(location + "<-- Location");
+			if (location.charAt(0) != 'c' && location.charAt(0) != 'f' && location.charAt(0) != 'w' && location.charAt(0) != 'v')
+				System.out.println("Please choose a valid location!");
+				
+				
+		} while (location.charAt(0) != 'c' && location.charAt(0) != 'f' &&
+				 location.charAt(0) != 'w' && location.charAt(0) != 'v');
+				 
+		if (location.charAt(0) == 'c')
+			location = "cave";
+		else if (location.charAt(0) == 'f')
+			location = "forest";
+		else if (location.charAt(0) == 'w')
+			location = "wasteland";
+		else if (location.charAt(0) == 'v')
+			location = "village";
+			
+		// getting a valid (non-duplicate) name for the monster
+		boolean openName = false;
+		ArrayList <String> currentMonsters = new ArrayList <> ();
+		while (readFile.hasNext()) {
+				String line = readFile.nextLine();
+				currentMonsters.add(line);
+		}
+		readFile.close();
+		
+		while (openName == false) {
+			System.out.print("Enter a name for this " + location + " monster: ");
+			name = s.nextLine();
+			openName = true;
+			for (int i = 0; i < currentMonsters.size(); i++) {
+				String[] pieces = currentMonsters.get(i).split(":");
+				if (name.equalsIgnoreCase(pieces[3])) {
+					System.out.println("Monster already exists! Enter a different name.");
+					openName = false;
+				}
+			}
+		}
+		
+		// getting valid monster health
+		boolean validHealth = false;
+		while (validHealth == false) {
+			try {
+				System.out.print("Enter this " + name + "'s health: ");
+				health = Integer.parseInt(s.nextLine());
+				validHealth = true;
+			} catch (NumberFormatException x) {
+				System.out.println("Please input only integers!");
+				validHealth = false;
+			}
+		}
+		
+		// getting valid monster damage
+		boolean validDamage = false;
+		while (validDamage == false) {
+			try {
+				System.out.print("Enter this " + name + "'s damage: ");
+				damage = Integer.parseInt(s.nextLine());
+				validDamage = true;
+			} catch (NumberFormatException x) {
+				System.out.println("Please input only integers!");
+				validDamage = false;
+			}
+		}
+		
+		// getting valid monster difficulty
+		boolean validDifficulty = false;
+		while (validDifficulty == false) {
+			try {
+				System.out.print("Enter this " + name + "'s difficulty: ");
+				difficulty = Integer.parseInt(s.nextLine());
+				validDifficulty = true;
+			} catch (NumberFormatException x) {
+				System.out.println("Please input only integers!");
+				validDifficulty = false;
+			}
+		}
+		
+		
+		// writing to a file
+		System.out.println("Creating monster...");
+		String outputString = location + ":" + health + ":" + damage + ":" + name + ":" + difficulty;
+		FileWriter writeFile = new FileWriter(monsterFile, true);
+		writeFile.write("\n" + outputString);
+		writeFile.close();
+		System.out.println("Monster created! May God have mercy on us all.");
 		
 	}
 	
